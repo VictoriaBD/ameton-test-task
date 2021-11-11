6e46d6fbbfdcf11f9693f72c32bfb24b337ec610
Victoria <dmitrieva.vb.92@gmail.com>
2021-11-11 17:13:53 +0300

    Add files

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.faq_items').slick({
+		autoplay: false,
+		fade: false,
+		dots: false,
+		arrows: true,
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+});
